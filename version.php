cc5f12f070acb19f8dfe25a51b303e0de9327f8a
Don Hinkelman <hinkelman@mac.com>
2021-06-26 10:55:08 +0900

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die;
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_sharing_cart';
-$plugin->version = 2021062500;
+$plugin->version = 2021062600;
 $plugin->requires = 2018120300; // Moodle 3.6
-$plugin->release = '3.10, release 2';
+$plugin->release = '3.10, release 3';
 $plugin->maturity = MATURITY_STABLE;
