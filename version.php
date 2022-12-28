67de6d83f72fa1e0a45a54ca3adcdfd389707206
Mikkel Jeppesen <42199572+Mikkeljeppesen1@users.noreply.github.com>
2022-12-28 09:11:38 +0100

    Updated requires to moodle 4.0

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die;
 /** @var stdClass $plugin */
 $plugin->component = 'block_sharing_cart';
 $plugin->version   = 2022122000;
-$plugin->requires  = 2021051704; // Moodle 3.11.4
+$plugin->requires  = 2022041900; // Moodle 4.0
 $plugin->release   = '4.0, release 4';
 $plugin->maturity  = MATURITY_STABLE;
