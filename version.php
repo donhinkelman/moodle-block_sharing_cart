e99376493ed18de9fb3efb72ecee2cc7d5073542
Anders Rasmussen <ara@praxis.dk>
2023-03-23 10:24:16 +0100

    Updated version.php

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die;
 /** @var object $plugin */
 $plugin->component = 'block_sharing_cart';
 $plugin->version   = 2023032300;
-$plugin->requires  = 2021051704; // Moodle 3.11.4
+$plugin->requires  = 2022112800; // Moodle 4.1.0
 $plugin->release   = '4.1, release 1';
 $plugin->maturity  = MATURITY_STABLE;
