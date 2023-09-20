0f537c96191dd186b641412e9a2615ed3e535b3b
Anders Rasmussen <ara@praxis.dk>
2023-09-20 09:32:58 +0200

    #102 - Added activity copy button, if user has capability to back up activities, but not to manage activities

--- settings.php
+++ settings.php
@@ -65,4 +65,12 @@ if ($ADMIN->fulltree) {
             1,
         )
     );
+    $settings->add(
+        new admin_setting_configcheckbox(
+            'block_sharing_cart/show_copy_activity_in_block',
+            get_string('settings:show_copy_activity_in_block', 'block_sharing_cart'),
+            get_string('settings:show_copy_activity_in_block_desc', 'block_sharing_cart'),
+            1,
+        )
+    );
 }
