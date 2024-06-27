1ea594cba1e4c14354377965fca282cd4fe1de40
Frederik Milling Pytlick <fmp@praxis.dk>
2024-06-27 11:47:29 +0200

    #137 - Updated javascript to handle wrong type

--- amd/src/app/block/queue/element.js
+++ amd/src/app/block/queue/element.js
@@ -30,6 +30,11 @@ export default class QueueElement {
     #loadQueuePromise = null;
 
     /**
+     * @type {Object|null}
+     */
+    #loadQueueToken = null;
+
+    /**
      * @type {boolean}
      */
     #preventReload = false;
@@ -71,113 +76,127 @@ export default class QueueElement {
         if (this.#loadQueuePromise !== null) {
             this.#loadQueuePromise.then(() => {
                 this.tryReloadQueue();
+            }).catch(() => {
+                this.tryReloadQueue();
             });
             return;
         }
 
-        this.#loadQueuePromise = this.loadQueue();
+        this.#loadQueueToken = {};
+        this.#loadQueuePromise = this.loadQueue(false, this.#loadQueueToken);
         this.#loadQueuePromise.then(() => {
             this.#loadQueuePromise = null;
+            this.#loadQueueToken = null;
         }).catch(() => {
+            this.#loadQueueToken = null;
             this.#loadQueuePromise = null;
         });
     }
 
     /**
      * @param {Boolean} showSpinner
+     * @param {Object} token
      * @return {Promise<void>}
      */
-    async loadQueue(showSpinner = false) {
-        const oldChildren = this.#element.children;
-        const oldQueueItemsCount = this.getQueueItems().length;
+    async loadQueue(showSpinner = false, token = {}) {
+        return new Promise(async(resolve, reject) => {
+            token.abort = () => {
+                reject();
+            };
 
-        if (showSpinner) {
-            this.#element.innerHTML = '<i class="fa fa-spinner"></i>';
-        }
+            const oldChildren = this.#element.children;
+            const oldQueueItemsCount = this.getQueueItems().length;
 
-        const elements = await this.#baseFactory.moodle().template().createElementsFromFragment(
-            'block_sharing_cart',
-            'item_queue',
-            M.cfg.contextid,
-            {}
-        );
-
-        const queueItems = elements.filter((element) => {
-            if (!(element instanceof Element)) {
-                return false;
+            if (showSpinner) {
+                this.#element.innerHTML = '<i class="fa fa-spinner"></i>';
             }
 
-            return element.classList.contains('queue-item');
-        });
+            const elements = await this.#baseFactory.moodle().template().createElementsFromFragment(
+                'block_sharing_cart',
+                'item_queue',
+                M.cfg.contextid,
+                {}
+            );
 
-        if (oldQueueItemsCount > queueItems.length) {
-            const removedElements = Array.from(oldChildren).filter((element) => {
-                const correspondingElement = queueItems.find((el) => el.dataset.id === element.dataset.id);
-
-                return correspondingElement === undefined;
-            });
-
-            const sectionIds = [];
-            removedElements.forEach((element) => {
-                const sectionId = element.dataset.toSectionId;
-
-                if (!sectionId) {
-                    return;
+            const queueItems = elements.filter((element) => {
+                if (!(element instanceof Element)) {
+                    return false;
                 }
 
-                if (sectionIds.indexOf(sectionId) !== -1) {
-                    return;
-                }
-
-                sectionIds.push(sectionId);
+                return element.classList.contains('queue-item');
             });
 
-            if (sectionIds.length > 0) {
-                this.#reactive.dispatch('sectionState', sectionIds).then(() => {
-                    Toast.add(getString('you_may_need_to_reload_the_course_warning', 'block_sharing_cart'), {
-                        closeButton: true,
-                        autohide: false,
-                        type: 'warning'
-                    });
+            if (oldQueueItemsCount > queueItems.length) {
+                const removedElements = Array.from(oldChildren).filter((element) => {
+                    const correspondingElement = queueItems.find((el) => el.dataset.id === element.dataset.id);
+
+                    return correspondingElement === undefined;
                 });
+
+                const sectionIds = [];
+                removedElements.forEach((element) => {
+                    const sectionId = element.dataset.toSectionId;
+
+                    if (!sectionId) {
+                        return;
+                    }
+
+                    if (sectionIds.indexOf(sectionId) !== -1) {
+                        return;
+                    }
+
+                    sectionIds.push(sectionId);
+                });
+
+                if (sectionIds.length > 0) {
+                    this.#reactive.dispatch('sectionState', sectionIds).then(() => {
+                        Toast.add(getString('you_may_need_to_reload_the_course_warning', 'block_sharing_cart'), {
+                            closeButton: true,
+                            autohide: false,
+                            type: 'warning'
+                        });
+                    });
+                }
             }
-        }
 
-        this.#element.innerHTML = '';
+            this.#element.innerHTML = '';
 
-        queueItems.forEach((element) => {
-            const runNowButton = element.querySelector('button.btn');
+            queueItems.forEach((element) => {
+                const runNowButton = element.querySelector('button.btn');
 
-            if (!runNowButton) {
-                return;
-            }
-
-            runNowButton.addEventListener('click', () => {
-                const taskId = element.dataset.id;
-
-                runNowButton.disabled = true;
-
-                this.#preventReload = true;
-                if (this.#loadQueuePromise !== null) {
-                    this.#loadQueuePromise.abort();
+                if (!runNowButton) {
+                    return;
                 }
 
-                Ajax.call([{
-                    methodname: 'block_sharing_cart_run_task_now',
-                    args: {
-                        task_id: taskId
+                runNowButton.addEventListener('click', () => {
+                    const taskId = element.dataset.id;
+
+                    runNowButton.disabled = true;
+
+                    this.#preventReload = true;
+                    if (this.#loadQueueToken !== null) {
+                        this.#loadQueueToken.abort();
                     }
-                }]);
 
-                setTimeout(() => {
-                    this.#preventReload = false;
-                    this.tryReloadQueue(true);
-                }, 2000);
-            }, {once: true});
-        });
+                    Ajax.call([{
+                        methodname: 'block_sharing_cart_run_task_now',
+                        args: {
+                            task_id: taskId
+                        }
+                    }]);
 
-        elements.forEach((element) => {
-            this.#element.appendChild(element);
+                    setTimeout(() => {
+                        this.#preventReload = false;
+                        this.tryReloadQueue(true);
+                    }, 2000);
+                }, {once: true});
+            });
+
+            elements.forEach((element) => {
+                this.#element.appendChild(element);
+            });
+
+            resolve();
         });
     }
 }
